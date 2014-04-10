e439decf910b95a93926a2da33a720ac93ea358d
Bryan Richter <b@chreekat.net>
2014-04-09 17:15:20 -0700

    Rewrite notify_me

--- irssi/scripts/notify_me.pl
+++ irssi/scripts/notify_me.pl
@@ -3,6 +3,8 @@ use vars qw($VERSION %IRSSI);
 
 use Irssi;
 
+use Text::Wrap;
+
 my $nick = "chreekat";
 
 $VERSION = '0.1';
@@ -13,30 +15,31 @@ $VERSION = '0.1';
     description => "Uses notify-send when things show up.",
     license => "WTFPL",
     url => "",
-    changed => "15 July 2013",
+    changed => "14 Jan 2014",
+    modules => 'Text::Wrap',
 );
 
+sub notify {
+    my ($hdr, $msg) = @_;
+    system( "notify-send", $hdr, $msg);
+}
+
 sub notify_me {
-    my $msg = $_[1];
-    if ($ENV{DISPLAY}) {
-       if (    # my name!
-               $msg =~ /$nick/i ||
-               # a question (but not one directed to someone else)
-               # $msg =~ /\?/ && $msg !~ /^\S*:/ ||
-               # word wars for nanowrimo
-               $msg =~ /word war/) {
-           system "notify-send", "$_[2]:", "$msg";
-        }
+    my ($srv, $msg, $sender, $senderAddr, $window) = @_;
+
+    #Irssi::print "srv: $srv\nmsg: $msg\nnick: $nick\nsenderAddr: $senderAddr\nwindow: $window\n";
+    my $to;
+    ($to, $msg) = split(/: /, $msg, 2);
+    ($msg, $to) = ($to, $msg) unless $msg or not $to;
+    #Irssi::print "msg: $msg\nto: $to\n";
+
+    $window =~ s/.+/[$&]/;
+
+    if (!$window or $to eq $nick) {
+        notify("$sender $window", "$msg");
     }
-    return;
 }
-
-sub notify_priv {
-    my $data = $_[1];
-    notify_me $data;
-}
-
 
 Irssi::signal_add 'message public', 'notify_me';
-Irssi::signal_add 'event privmsg', 'notify_priv';
+Irssi::signal_add 'message private', 'notify_me';
 
