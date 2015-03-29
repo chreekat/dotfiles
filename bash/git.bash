28e56e21b9b9fd3340d176c78df29272511d7b73
Bryan Richter <b@chreekat.net>
2015-03-28 19:21:10 -0700

    Alias tweaks

--- bash/git.bash
+++ bash/git.bash
@@ -2,31 +2,39 @@
 # variable reuse. (And hey, four less characters to type.)
 
 log_aliases () {
-    gl='git log --date=short'
-    line='%C(10)%h │%Creset %s%C(auto)%d%C(10) │ %cd, %an%Creset'
+    gl='git log --color=always --date=short'
+    line='%C(10)%h │%Creset %C(4)%s%Creset%C(auto).%d%C(10) %cd, %an%Creset'
     std_format="--pretty=tformat:'$line'"
     big_break='%C(13 bold)********************************************************************************%C(reset)%n'
     break_format="--pretty=tformat:'$big_break$line'"
-    almost_all="--branches --remotes" # But not tags
 
 
 
-    alias lg="$gl     --graph $almost_all --first-parent        $std_format"
-    alias lgs="$gl    --graph $almost_all --first-parent --stat $std_format"
-    alias lgf="$gl    --graph $almost_all                       $std_format"
-    alias lga="$gl    --graph             --first-parent        $std_format"
+    alias lgg="$gl     --graph --branches --first-parent        $std_format"
+    alias lgs="$gl    --graph --branches --first-parent --stat $std_format"
+    alias lgff="$gl    --graph --branches                       $std_format"
+    alias lgsff="$gl   --graph --branches                --stat $std_format"
+    alias lgaa="$gl    --graph             --first-parent        $std_format"
+    alias lgaff="$gl   --graph                                   $std_format"
     alias lp="$gl  -p                     --first-parent        $break_format"
-    alias lpf="$gl -p                                           $break_format"
+    alias lpff="$gl -p                                           $break_format"
+    alias lg="lgg | head -n 20"
+    alias lgss="lgs | head -n 20"
+    alias lgf="lgff | head -n 20"
+    alias lgsf="lgsff | head -n 20"
+    alias lga="lgaa | head -n 20"
+    alias lgaf="lgaff | head -n 20"
+    alias lpp="lp | head -n 20"
+    alias lpf="lpff | head -n 20"
 
-    alias lg1="$gl    --graph $almost_all --first-parent        $std_format | head -n 10"
+    alias lg1="$gl    --graph --branches --first-parent        $std_format | head -n 10"
 }
 
 log_aliases
 
-alias pl="git pull --ff-only"
-
-br-clone () {
-  repo=$1
-  shift
-  git clone $repo -o barobo -c author.email=bryan@barobo.com $@
+# FIXME wtf is this? Did I write this?
+addsubrepo () {
+  git submodule add $(git -C $1 remote -v | cut -f2| grep -v fetch | cut -f1 -d' ')
 }
+
+alias regit="source ~/.bash/git.bash"
