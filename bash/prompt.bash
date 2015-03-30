359553b59df53f43ce03cf6650593efea58f4748
Bryan Richter <b@chreekat.net>
2015-03-30 09:54:21 -0700

    Updates!??!?!

---
+++ bash/prompt.bash
@@ -0,0 +1,47 @@
+__p_setprompt () {
+  showLastRet=
+  if [ $lastRet -ne 0 ]; then
+    showLastRet="\[\e[31m\]($lastRet) \[\e[0m\]"
+  fi
+
+  showJobs=
+  js=$(jobs | grep ^\\[ | sed -e 's/ \+/ /g' | cut -f 3 -d' ')
+  if [ -n "$js" ]; then
+    showJobs=" \[\e[0;38;5;60m\][$(echo $js | sed -e 's/ /, /g')]\[\e[0m\]"
+  fi
+
+  systemColor="1;38;5;34m"
+  if [ -n "$SSH_TTY" ]; then
+    # Different color if we're logged in remotely
+    systemColor="32m"
+  fi
+
+  sandbox=$(__p_wutSandbox)
+
+  PS1="\[\e]0;\w\a\]${showLastRet}\[\e[${systemColor}\]\u@\h \[\e[0;33m\]\w\[\e[0m\]${showJobs}\$(__git_ps1) ${sandbox}\n\$ "
+}
+
+__p_wutSandbox () {
+    sandbox=
+    # Symlinks can be weird so let's just bail
+    if git root >/dev/null 2>&1 && echo $PWD | grep -q $(git root)
+    then
+        ldir=$PWD
+        git_root=$(git root)
+        while [ $ldir != $git_root ]
+        do
+            if ls $ldir/*.cabal >/dev/null 2>&1
+            then
+                if [ ! -e "$ldir/cabal.sandbox.config" ]
+                then
+                    sandbox="\[\e[31m(no sandbox)\[\e[0m"
+                fi
+                break
+            fi
+            ldir=$(dirname $ldir)
+        done
+    fi
+    echo $sandbox
+}
+
+export PROMPT_COMMAND='lastRet=$?; __p_setprompt; unset lastRet'
---
+++ bash/snowdrift.bash
@@ -0,0 +1,7 @@
+mr () {
+    xdg-open https://gitorious.org/snowdrift/snowdrift/merge_requests/$1
+}
+
+sd () {
+    xdg-open https://snowdrift.coop/p/snowdrift/t/$1
+}
