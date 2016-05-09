590f1ba01c79eea6f1d749f29f26258c26ba1201
Bryan Richter <b@chreekat.net>
2016-05-09 10:07:59 -0700

    Add db marker

--- bash/prompt.bash
+++ bash/prompt.bash
@@ -1,3 +1,19 @@
+__p_dbstat=
+__p_setdbstat () {
+  if [ -d .postgres-work ]; then
+      __p_dbstat=" (\[\e[31m\]db✗\[\e[0m\])"
+      pidfile=.postgres-work/data/postmaster.pid
+      if [ -e $pidfile ]; then
+          pid=$(head -n1 $pidfile)
+          if ps $pid >/dev/null 2>&1; then
+              __p_dbstat=" (\[\e[32m\]db✓\[\e[0m\])"
+          fi
+      fi
+  else
+    unset __p_dbstat
+  fi
+}
+
 __p_setprompt () {
   showLastRet=
   if [ $lastRet -ne 0 ]; then
@@ -16,8 +32,9 @@ __p_setprompt () {
     systemColor="32m"
   fi
 
+  __p_setdbstat
   delim="\[\e[90;45m\] \[\e[0m\]"
-  PS1="${delim} \[\e]0;\w\a\]${showLastRet}\[\e[${systemColor}\]\u@\h \[\e[0;33m\]\w\[\e[0m\]${showJobs}\$(__git_ps1) ${delim} "
+  PS1="${delim} \[\e]0;\w\a\]${showLastRet}\[\e[${systemColor}\]\u@\h \[\e[0;33m\]\w\[\e[0m\]${showJobs}\$(__git_ps1)${__p_dbstat} ${delim} "
 }
 
 export PROMPT_COMMAND='lastRet=$?; __p_setprompt; unset lastRet'
