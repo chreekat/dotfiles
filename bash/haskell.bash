d66540d415e3198ae395bf674fdc4120c1f0aad1
Bryan Richter <b@chreekat.net>
2016-09-30 11:14:19 -0700

    Typo

--- bash/haskell.bash
+++ bash/haskell.bash
@@ -1,3 +1,3 @@
 alias git-fast-tags="git ls-tree -r HEAD --name-only | grep -E '*.l?hs$' | xargs fast-tags -v"
 alias git-hothasktags="git ls-tree -r HEAD --name-only | grep -E '*.l?hs$' | xargs hothasktags -X TemplateHaskell -X ScopedTypeVariables -X LambdaCase -X RecordWildCards -X MultiWayIf -X CPP -X TypeFamilies -X TupleSections -X QuasiQuotes -O tags"
-alias hoogle-local-srv="stack -- hoogle server --database=.stack-work/hoogle  --local -p 5555 -q"
+alias hoogle-local-srv="stack hoogle -- server --local -p 5555 -q"
