cb1c0352bfd23791eb175fa2aecfbd24c362e257
Bryan Richter <b@chreekat.net>
2019-01-05 17:13:48 +0200

    No mouse follow, better layouts

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -1,9 +1,39 @@
-import XMonad
-import qualified Data.Map as M
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+import           Data.Foldable   (msum)
+import qualified Data.Map        as M
+import           XMonad
+import qualified XMonad.StackSet as W
+
+-- | A custom Tall that doesn't paradoxically allow zero windows in the main
+-- space.
+newtype MinOneMain a = MinOneMain (Tall a)
+    deriving (Show, Read)
+
+instance LayoutClass MinOneMain a where
+    pureLayout (MinOneMain (Tall nmain _ frac)) r s = zip ws rs
+        where
+            ws = W.integrate s
+            rs = tile frac r nmain (length ws)
+
+    pureMessage (MinOneMain (Tall nmain delta frac)) m =
+        msum
+            [ fmap (MinOneMain . resize) (fromMessage m)
+            , fmap (MinOneMain . incMain) (fromMessage m)]
+
+        where
+            resize Shrink = Tall nmain delta (max (1/100) $ frac-delta)
+            resize Expand = Tall nmain delta (min (99/100) $ frac+delta)
+            incMain (IncMasterN d) = Tall (max 1 (nmain+d)) delta frac
+
+    description _ = "Paradox-free Tall"
 
 main = xmonad defaultConfig
     { modMask = mod4Mask
     , terminal = "urxvtc -ls"
+    , clickJustFocuses = True
+    , focusFollowsMouse = False
+    , layoutHook = MinOneMain (Tall 1 (3/100) (53/100)) ||| Full
     , keys = myKeys
     }
 
