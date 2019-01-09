ead52686b2d0b00bb1519fa4829ed0ed18f186b6
Bryan Richter <bryan.richter@relexsolutions.com>
2019-01-09 17:00:24 +0200

    Add vbox and docker services for RELEX dev

--- nixos/configuration-bryan-laptop.nix
+++ nixos/configuration-bryan-laptop.nix
@@ -2,11 +2,13 @@
 
 {
   networking.hostName = "bryan-laptop";
+  networking.search = [ "relex.fi" "relexsolutions.com" ];
   system.stateVersion = "18.09";
 
   imports =
     [ /etc/nixos/hardware-configuration.nix
       /etc/nixos/system-common.nix
+      #/etc/nixos/relex-vpn.nix
     ];
 
   # LUKS
@@ -17,4 +19,23 @@
       preLVM = true;
     }
   ];
+
+  virtualisation = {
+    virtualbox.host = {
+      enable = true;
+      headless = true;
+    };
+    docker.enable = true;
+  };
+
+  users.extraUsers.b = {
+    extraGroups = ["vboxusers" "docker"];
+  };
+
+  # Relex VPN
+  #networking.relexVpn = {
+  #  enable = true;
+  #  ipaUsername = "bryan.richter";
+  #  sharedSecret = "psittacines";
+  #};
 }
