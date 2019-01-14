4883b8774e9e5fb1937f9432006e535939d69b76
Bryan Richter <bryan.richter@relexsolutions.com>
2019-01-14 13:38:39 +0200

    Remove bad weird DP2

--- screenlayout/work.sh
+++ screenlayout/work.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output DP2-8 --off --output VIRTUAL1 --off --output eDP1 --off --output DP1 --off --output DP2-1 --off --output HDMI1 --off --output HDMI2 --off --output DP1-1 --mode 1920x1200 --pos 1440x0 --rotate normal --output DP1-8 --mode 2560x1440 --pos 0x0 --rotate left --output DP2 --off
+xrandr --output VIRTUAL1 --off --output eDP1 --off --output DP1 --off --output HDMI1 --off --output HDMI2 --off --output DP1-1 --mode 1920x1200 --pos 1440x0 --rotate normal --output DP1-8 --mode 2560x1440 --pos 0x0 --rotate left
