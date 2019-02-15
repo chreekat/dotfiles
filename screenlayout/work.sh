581a1b61072ed1a83dc82bce61cfe2b1ed34dfb0
Bryan Richter <bryan.richter@relexsolutions.com>
2019-02-15 11:58:52 +0200

    Use sleeps to avoid failures

--- screenlayout/work.sh
+++ screenlayout/work.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
-xrandr --output VIRTUAL1 --off --output eDP1 --off --output DP1 --off --output DP1-1 --mode 1920x1200 --pos 1440x456 --rotate normal --output HDMI2 --off --output HDMI1 --off --output DP1-8 --primary --mode 2560x1440 --pos 0x0 --rotate left --output DP2 --off
+xrandr --output DP1-1 --mode 1920x1200 --pos 2560x0 --rotate normal
+sleep 1
+xrandr --output DP1-8 --primary --mode 2560x1440 --pos 0x0 --rotate normal
+sleep 1
+xrandr --output eDP1 --off
