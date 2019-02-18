c8eed2a0d0997cb912b08f967582f2cb079c3f43
Bryan Richter <bryan.richter@relexsolutions.com>
2019-02-18 18:53:42 +0200

    Build a better battery

--- tmux/bat.sh
+++ tmux/bat.sh
@@ -1,7 +1,18 @@
 #!/usr/bin/env bash
 
+acSym=''
+if [ "$(cat /sys/class/power_supply/AC/online)" = 1 ]; then
+    acSym='+'
+else
+    acSym='-'
+fi
+
+echo -n "$acSym"
+
 cat \
     /sys/class/power_supply/BAT0/charge_full \
     /sys/class/power_supply/BAT0/charge_now \
-    <(echo 100*r/p) \
+    <(echo 100*r/n) \
     | dc
+
+echo %
