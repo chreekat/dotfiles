76ba35ec1ba0a563380e35585d20a5358fc6caaa
Bryan Richter <b@chreekat.net>
2019-02-24 16:49:36 +0200

    Make bat.h work on both laptops

--- tmux/bat.sh
+++ tmux/bat.sh
@@ -1,7 +1,15 @@
 #!/usr/bin/env bash
 
-cat \
-    /sys/class/power_supply/BAT0/charge_full \
-    /sys/class/power_supply/BAT0/charge_now \
-    <(echo 100*r/p) \
-    | dc
+datum () {
+    echo "/sys/class/power_supply/BAT0/${1}_${2}"
+}
+
+sys_datum () {
+    if [ -f "$(datum charge full)" ]; then
+        datum charge $1
+    else
+        datum energy $1
+    fi
+}
+
+cat "$(sys_datum full)" "$(sys_datum now)" <(echo 100*r/p) | dc
