87923df55f66ef948705ffe67eac25d6f275560d
Bryan Richter <bryan.richter@relexsolutions.com>
2019-03-26 19:45:28 +0200

    relex updates

--- nixos/configuration-bryan-laptop.nix
+++ nixos/configuration-bryan-laptop.nix
@@ -6,6 +6,7 @@
     ./system-common.nix
     /home/b/RELEX/the-repo/support/mordor-local-dev.module.nix
     ./relex-vpn.nix
+    ./relex-specific.nix
   ];
 
   # LUKS
@@ -17,15 +18,29 @@
     }
   ];
 
-  environment.systemPackages = with pkgs; [
-    ansible
-  ];
-
+  networking.hosts = {
+    # My PC environments
+    "10.23.12.64" = [
+      "bryan-legacy.relexsolutions.com"
+      "bryan-scale-out.relexsolutions.com"
+      "bryan-scale-out-nix.relexsolutions.com"
+    ];
+  };
   networking.hostName = "bryan-laptop";
   networking.search = [ "relex.fi" "relexsolutions.com" ];
+
+  nixpkgs.overlays = [ (import ./relex-overlays.nix) ];
+
+  users.users.b.extraGroups = ["docker"];
 
   relex.developer = "b";
   relex.vpn.username = "bryan.richter";
 
   system.stateVersion = "18.09";
+
+  # For running Degustacion
+  virtualisation.docker = {
+    enable = true;
+    enableOnBoot = false;
+  };
 }
