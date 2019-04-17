34aa1b43775ffc058cbad080dc4ef3f33afdef23
Bryan Richter <bryan.richter@relexsolutions.com>
2019-04-17 12:40:50 +0300

    Alphabetize

--- nixos/configuration-bryan-laptop.nix
+++ nixos/configuration-bryan-laptop.nix
@@ -4,8 +4,8 @@
   imports = [
     /etc/nixos/hardware-configuration.nix
     ./system-common.nix
-    /home/b/RELEX/the-repo/support/mordor-local-dev.module.nix
     ./relex-vpn.nix
+    ./mats-vpnc-service.nix
   ];
 
   # LUKS
@@ -17,25 +17,47 @@
     }
   ];
 
-  networking.hosts = {
-    # My PC environments
-    "10.23.12.64" = [
-      "bryan-legacy.relexsolutions.com"
-      "bryan-scale-out.relexsolutions.com"
-      "bryan-scale-out-nix.relexsolutions.com"
-    ];
+  networking = {
+    hostName = "bryan-laptop";
+    hosts = {
+      # My PC environments
+      "10.23.12.64" = [
+        "bryan-legacy.relexsolutions.com"
+        "bryan-scale-out.relexsolutions.com"
+        "bryan-scale-out-nix.relexsolutions.com"
+      ];
+    };
+    search = [ "relex.fi" "relexsolutions.com" ];
   };
-  networking.hostName = "bryan-laptop";
-  networking.search = [ "relex.fi" "relexsolutions.com" ];
 
   nixpkgs.overlays = [ (import ./relex-overlays.nix) ];
-
-  users.users.b.extraGroups = ["docker"];
 
   relex.developer = "b";
   relex.vpn.username = "bryan.richter";
 
+  services = {
+    # RELEX policy
+    clamav = {
+      daemon.enable = true;
+      daemon.extraConfig = ''
+        ScanOnAccess yes
+        OnAccessIncludePath /home/b/Downloads
+      '';
+      updater.enable = true;
+    };
+    vpnc.servers = {
+      relex = {
+        gateway = "gp-vpn.relex.fi";
+        id = "linux";
+        secret = import ./vpnc-secret;
+        username = "bryan.richter@relexsolutions.com";
+      };
+    };
+  };
+
   system.stateVersion = "18.09";
+
+  users.users.b.extraGroups = ["docker"];
 
   # For running Degustacion
   virtualisation.docker = {
