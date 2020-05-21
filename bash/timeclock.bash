d16314dc2d80f2652d1a39d0c22fd32d4b8ac42e
Bryan Richter <bryan.richter@relexsolutions.com>
2020-05-21 14:03:14 +0300

    Add timeclock stuff

---
+++ bash/timeclock.bash
@@ -0,0 +1,21 @@
+#
+# hledger timetracking stuff
+#
+
+export TIMELOG=~/.timeclock
+
+to () {
+    echo o `date "+%Y-%m-%d %H:%M:%S"` >> $TIMELOG
+}
+
+ti () {
+    # First argument is account; two spaces separate it from the description.
+    # Hence the "" arg, which makes the spaces happen.
+    if ( tail -n 1 $TIMELOG | grep -q ^i )
+    then
+        to
+    fi
+    echo i `date "+%Y-%m-%d %H:%M:%S"` $1 ${2:+""} ${@:2} >> $TIMELOG
+}
+
+alias tck='hledger -f $TIMELOG'
