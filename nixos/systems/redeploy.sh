774b4852d8bc716553a9871b6e23378b6805ff16
Bryan Richter <b@chreekat.net>
2023-11-01 13:28:23 +0200

    Add redeploy.sh

---
+++ nixos/systems/redeploy.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env bash
+
+set -Eeuo pipefail
+
+# Usage:
+#        ./redeploy.sh matrix-builder
+
+# Redeploys a server. Enforces that the last-deployed state is recorded in this
+# file. This might be more pain than it's worth, but the idea was to keep track
+# of deployment conflicts since there are multiple people who do them. Better
+# ideas welcome.
+
+# Since this deploys to a remote server, /etc/nixos is unused on that server.
+# Consider placing placeholder-for-redeploy.sh.nix in
+# /etc/nixos/configuration.nix to avoid calamity.
+
+declare -A old target
+
+# Change this if you want to deploy.
+old=(
+    "puny"
+    /nix/store/m0sm4v64w6am6qw3z60ycqcfr5q320s0-nixos-system-puny-23.05.20231029.3e10c80
+)
+
+target=(
+    "puny" puny
+)
+
+rebuild () {
+    set -Eeuo pipefail
+    system="$1"
+    shift
+    nixos-rebuild --use-remote-sudo --use-substitutes --flake ".#${system}" "$@"
+}
+
+rm ../mods/syncthing.nix
+gpg ../mods/syncthing.nix.asc
+trap '> ../mods/syncthing.nix' EXIT
+
+rebuild $1 build
+
+new="$(readlink result)"
+
+if [[ ${old["$1"]} != $new ]]; then
+    >&2 echo
+    >&2 echo "*** This is a NEW configuration. Edit $0 if you're satisfied with it."
+    >&2 echo
+    >&2 echo "*** New result for $1: $(readlink result)"
+    exit 1
+else
+    rebuild "$1" switch --target-host "${target["$1"]}"
+fi
