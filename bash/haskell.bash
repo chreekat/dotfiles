855fcbccf0f9ce0c6fdcb4fbcf3675e7069b1643
Bryan Richter <b@chreekat.net>
2024-07-09 09:31:43 +0300

    Fix git-fast-tags

--- bash/haskell.bash
+++ bash/haskell.bash
@@ -1,2 +1,2 @@
-alias git-fast-tags="git ls-tree -r HEAD --name-only | grep -E '*.l?hs$' | xargs fast-tags -v"
+alias git-fast-tags="git ls-tree -r HEAD --name-only | grep -E '\.l?hs$' | xargs fast-tags -v"
 alias hoogle-local-srv="stack hoogle -- server --local -p 5555 -q >/dev/null 2>&1 &"
