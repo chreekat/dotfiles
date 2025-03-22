955025507954d258a01061304783da92b2c6ebc5
Bryan Richter <b@chreekat.net>
2025-03-22 14:44:18 +0200

    Automatically set tlp settings

--- nixos/mods/laptop.nix
+++ nixos/mods/laptop.nix
@@ -1,3 +1,8 @@
 {
   services.tlp.enable = true;
+  # Keep charge betwene 80-82% when plugged in. Saves battery life.
+  services.tlp.settings = {
+    START_CHARGE_THRESH_BAT0 = 80;
+    STOP_CHARGE_THRESH_BAT0 = 82;
+  };
 }
