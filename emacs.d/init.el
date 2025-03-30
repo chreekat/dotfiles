36584613859663405441f2380c0a113c06682695
Bryan Richter <b@chreekat.net>
2025-03-30 19:20:29 +0300

    Emacs whatever

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -2,6 +2,11 @@
   (require 'use-package))
 (setq use-package-always-ensure t)
 
+; delete extraneous whitespace on save
+(use-package ws-butler
+  :config
+  (ws-butler-global-mode 1))
+
 ;; Cleanup the UI
 (menu-bar-mode -1)
 (tool-bar-mode -1)
@@ -14,6 +19,12 @@
 
 (global-set-key (kbd "C-c c") #'org-capture)
 (global-set-key (kbd "C-c a") #'org-agenda)
+
+; Save automatically, frequently, for SyncThing's benefit.
+(add-hook 'auto-save-hook 'org-save-all-org-buffers)
+
+; enable auto-fill-mode on org buffers
+(add-hook 'org-mode-hook 'turn-on-auto-fill)
 
 (setq
     org-startup-folded t
@@ -69,40 +80,59 @@
  '(inhibit-startup-screen t)
  '(org-agenda-custom-commands
    '(("z" "All categories"
-      ((tags-todo "+@haskell_foundation-secondary-SCHEDULED>\"<tomorrow>\"/TODO" nil)
-       (tags-todo "+@centralapp-secondary-SCHEDULED>\"<tomorrow>\"/TODO" nil)
-       (tags-todo "+@personal-secondary-SCHEDULED>\"<tomorrow>\"/TODO" nil)
-       (tags-todo "+@freelance-secondary-SCHEDULED>\"<tomorrow>\"/TODO" nil)
-       (tags-todo "+@housework-secondary-SCHEDULED>\"<tomorrow>\"/TODO" nil)
-       (tags-todo "+@volunteering-secondary-SCHEDULED>\"<tomorrow>\"/TODO" nil))
-      nil)
-     ("U" "Un-labeled TODOs" tags-todo
-      #("-{@.*}-secondary/TODO" 1 6
-        (regexp t))
-      nil)
+      ((tags-todo
+        "+@haskell_foundation-secondary-SCHEDULED>\"<tomorrow>\"/TODO" nil)
+       (tags-todo
+        "+@centralapp-secondary-SCHEDULED>\"<tomorrow>\"/TODO" nil)
+       (tags-todo "+@personal-secondary-SCHEDULED>\"<tomorrow>\"/TODO"
+                  nil)
+       (tags-todo
+        "+@freelance-secondary-SCHEDULED>\"<tomorrow>\"/TODO" nil)
+       (tags-todo
+        "+@housework-secondary-SCHEDULED>\"<tomorrow>\"/TODO" nil)
+       (tags-todo
+        "+@volunteering-secondary-SCHEDULED>\"<tomorrow>\"/TODO" nil))
+      ((org-agenda-sorting-strategy '(priority-down))))
+     ("U" "Un-labeled TODOs" tags-todo "-{@.*}-secondary/TODO" nil)
      ("P" "Active projects" tags-todo "-secondary/PROJ" nil)
      ("W" "Waiting for..." tags-todo "-secondary/WAIT" nil)))
  '(org-agenda-files
-   '("~/Syncthing/PhoneFiles/org/archive/2025-archive.org" "/home/b/Syncthing/PhoneFiles/org/books-to-read.org" "/home/b/Syncthing/PhoneFiles/org/thinking-about.org" "/home/b/Syncthing/PhoneFiles/org/maybe.org" "/home/b/Syncthing/PhoneFiles/org/Inbox.org" "/home/b/Syncthing/PhoneFiles/org/Tickler.org" "/home/b/Syncthing/PhoneFiles/org/agenda.org" "/home/b/Syncthing/PhoneFiles/org/projects.org" "/home/b/Syncthing/PhoneFiles/org/reference.org" "/home/b/Syncthing/PhoneFiles/org/todos.org" "/home/b/Syncthing/PhoneFiles/org/Projects/Haskell_Certification_Deployment.org" "/home/b/Syncthing/PhoneFiles/org/Projects/Stackage/Stackage.org"))
+   '("~/Syncthing/PhoneFiles/org/archive/2025-archive.org"
+     "/home/b/Syncthing/PhoneFiles/org/books-to-read.org"
+     "/home/b/Syncthing/PhoneFiles/org/thinking-about.org"
+     "/home/b/Syncthing/PhoneFiles/org/maybe.org"
+     "/home/b/Syncthing/PhoneFiles/org/Inbox.org"
+     "/home/b/Syncthing/PhoneFiles/org/Tickler.org"
+     "/home/b/Syncthing/PhoneFiles/org/agenda.org"
+     "/home/b/Syncthing/PhoneFiles/org/projects.org"
+     "/home/b/Syncthing/PhoneFiles/org/reference.org"
+     "/home/b/Syncthing/PhoneFiles/org/todos.org"
+     "/home/b/Syncthing/PhoneFiles/org/Projects/Haskell_Certification_Deployment.org"
+     "/home/b/Syncthing/PhoneFiles/org/Projects/Stackage/Stackage.org"))
  '(org-agenda-show-future-repeats nil)
  '(org-agenda-skip-deadline-if-done t)
  '(org-agenda-skip-scheduled-if-done t)
+ '(org-agenda-sticky t)
  '(org-agenda-tags-todo-honor-ignore-options t)
  '(org-agenda-todo-ignore-scheduled 'future)
  '(org-capture-bookmark nil)
  '(org-enforce-todo-dependencies t)
  '(org-export-backends '(ascii beamer html icalendar latex md odt))
+ '(org-indent-indentation-per-level 4)
  '(org-log-done 'time)
  '(org-log-into-drawer t)
  '(org-log-redeadline 'time)
  '(org-log-refile 'time)
  '(org-log-reschedule 'time)
  '(org-modules
-   '(ol-bbdb ol-bibtex ol-docview ol-doi ol-eww ol-gnus org-habit org-id ol-info ol-irc ol-mhe ol-rmail ol-w3m))
+   '(ol-bbdb ol-bibtex ol-docview ol-doi ol-eww ol-gnus org-habit org-id
+             ol-info ol-irc ol-mhe ol-rmail ol-w3m))
  '(org-priority-default 67)
  '(org-refile-targets '((org-agenda-files :maxlevel . 3)))
  '(org-refile-use-outline-path 'file)
+ '(org-startup-indented t)
  '(org-stuck-projects '("-secondary/PROJ" ("TODO") nil ""))
+ '(org-tags-column -85)
  '(org-todo-keyword-faces
    '(("PROJ" :foreground "blue" :weight bold)
      ("WAIT" :foreground "goldenrod" :weight normal))))
@@ -111,5 +141,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :extend nil :stipple nil :background "white" :foreground "black" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 120 :width normal :foundry "FSD " :family "PragmataPro Mono"))))
- )
+ '(default ((t (:inherit nil :extend nil :stipple nil :background "white" :foreground "black" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 120 :width normal :foundry "FSD " :family "PragmataPro Mono")))))
