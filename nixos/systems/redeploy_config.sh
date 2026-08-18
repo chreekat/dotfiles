92bb49a4e85999f7285e70ec9c178e185186112a
Bryan Richter <b@chreekat.net>
2026-08-18 16:38:47 +0300

    Remove unused syncthing dir

--- nixos/systems/redeploy_config.sh
+++ nixos/systems/redeploy_config.sh
@@ -2,7 +2,7 @@
 # shellcheck disable=SC2190
 old=(
     "puny"
-    /nix/store/wsl3w218kc91nm88phjl9df55djsppgw-nixos-system-puny-26.05.20260817.0dd31db
+    /nix/store/wl1ribl7dfsxplmx03a8yhdrwfdmhv3g-nixos-system-puny-26.05.20260817.0dd31db
     "honk"
     /nix/store/yljm183gn2dr3sflgayifdyzv0kpk8bz-nixos-system-honk-26.05.20260817.0dd31db
     "kuusi"
