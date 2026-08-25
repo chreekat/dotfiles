47a73a8494ed6c98442aa63b8e777d5b8ddb3ee0
Bryan Richter <b@chreekat.net>
2026-08-25 11:44:09 +0300

    More pin dialog shenanigans

--- nixos/mods/desktop.nix
+++ nixos/mods/desktop.nix
@@ -5,6 +5,13 @@
   services.gnome.gnome-keyring.enable = true;
   programs.seahorse.enable = true;
 
+  # A systemd user unit doesn't inherit /etc/set-environment, so gcr's
+  # ssh-agent needs SSH_ASKPASS handed to it directly.
+  systemd.user.services.gcr-ssh-agent = {
+    overrideStrategy = "asDropin";
+    environment.SSH_ASKPASS = "${pkgs.seahorse}/libexec/seahorse/ssh-askpass";
+  };
+
   xdg.portal = {
     enable = true;
     extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
